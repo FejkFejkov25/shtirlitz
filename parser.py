653132665da415032d3dda80ba62b7da9665dd30
FejkFejkov25 <fejkfejkov25@gmail.com>
2023-04-18 16:33:21 +0300

    update parser.py

--- parser.py
+++ parser.py
@@ -2,14 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-urls = [
+urls1 = [
         "https://www.anekdot.ru/tags/%D0%A8%D1%82%D0%B8%D1%80%D0%BB%D0%B8%D1%86", 
         "https://www.anekdot.ru/tags/%D0%A8%D1%82%D0%B8%D1%80%D0%BB%D0%B8%D1%86/2", 
         "https://www.anekdot.ru/tags/%D0%A8%D1%82%D0%B8%D1%80%D0%BB%D0%B8%D1%86/3", 
         "https://www.anekdot.ru/tags/%D0%A8%D1%82%D0%B8%D1%80%D0%BB%D0%B8%D1%86/4"
 ]
+url2 = "https://allanecdots.ru/stirlitz/"
 
-def parse(urls):
+
+def parse1(urls):
     jokes = []
 
     for url in urls:
@@ -24,8 +26,35 @@ def parse(urls):
     return jokes
 
 
-def main(): 
-    jokes = parse(urls)
+def parse2(url):
+    jokes = []
+    r = requests.get(url)
+    s = BeautifulSoup(r.text, "html.parser")
+    cs = BeautifulSoup(str(s).replace("<br>", '\n'), "html.parser") 
+    j = cs.find_all("p", class_="anekdot_body")
+    for joke in j:
+        jokes.append(joke.text)
+
+    for i in range(2, 11):
+        resp = requests.get(f"{url}{i}")
+        sp = BeautifulSoup(resp.text, "html.parser")
+        cleanSp = BeautifulSoup(str(sp).replace("<br>", '\n'), "html.parser")
+
+        jk = cleanSp.find_all("p", class_="anekdot_body")
+        for joke in jk:
+            jokes.append(joke.text)
+
+    return jokes
+
+def main():
+    jokes = []
+    part1 = parse1(urls1)
+    part2 = parse2(url2)
+    for i in part1:
+        jokes.append(i)
+    for i in part2:
+        jokes.append(i)
+
     total_jokes = len(jokes)
     db = sqlite3.connect("anekdoti.db")
     cursor = db.cursor()
